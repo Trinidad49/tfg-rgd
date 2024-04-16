02002f6195c4f10332283947b778b7b51b29eec6
Raul <raul.gonzalez1@alumnos.uneatlantico.es>
2024-04-16 11:31:44 +0200

    validacion en login

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -4,9 +4,34 @@ import { Link } from "react-router-dom";
 export const LoginForm = ({ onLogin }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleLogin = (e) => {
     e.preventDefault();
+    setEmailError("");
+    setPasswordError("");
+
+    if ("" === email) {
+      setEmailError("Please enter your email");
+      return;
+    }
+
+    if (!/^[\w-]+@([\w-])+[\w-]{2,4}$/.test(email)) {
+      setEmailError("Please enter a valid email");
+      return;
+    }
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
+    }
+
+    if (password.length < 7) {
+      setPasswordError("The password must be 8 characters or longer");
+      return;
+    }
+
     //Añadir logica para el login
     onLogin(email, password);
   };
@@ -22,6 +47,7 @@ export const LoginForm = ({ onLogin }) => {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+        <label>{emailError}</label>
       </div>
       <div>
         <input
@@ -31,8 +57,9 @@ export const LoginForm = ({ onLogin }) => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+        <label>{passwordError}</label>
       </div>
-      <button type="submit">Login</button>
+      <button onClick={handleLogin}>Login</button>
       <p>
         ¿No tienes cuenta? <Link to="/register">Registrate</Link>
       </p>
