ccd9313a0b152b0b3527e1a8ae05ead50d5a2912
Raul <raul.gonzalez1@alumnos.uneatlantico.es>
2024-05-04 13:58:46 +0200

    Style answerdata component

--- src/components/survey/AnswerData.jsx
+++ src/components/survey/AnswerData.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Typography } from "@mui/material";
+import { Card, CardContent, LinearProgress, Typography } from "@mui/material";
 
 export const AnswerData = ({ survey }) => {
   const [surveyData, setSurveyData] = useState(null);
@@ -30,38 +30,50 @@ export const AnswerData = ({ survey }) => {
     return <Typography>Loading... </Typography>;
   }
 
+  const getAnswerCount = (optionText, questionIndex) =>
+    surveyData.filter((a) => a.answers[questionIndex]?.answer === optionText)
+      .length;
+
   return (
     <div>
       <Typography variant="h4">{survey.title}</Typography>
       {survey.questions.map((question, index) => (
-        <div key={index}>
-          <Typography variant="h6">{question.text}</Typography>
-          {question.type === "text" ? (
-            <div>
-              {surveyData.map((answers, i) => (
-                <div key={i}>
-                  <Typography>{answers.answers[index].answer}</Typography>
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div>
-              {question.answers.map((option, optionIndex) => (
-                <div key={optionIndex}>
-                  <Typography>{option.text}</Typography>
-                  <Typography>
-                    Count:{" "}
-                    {
-                      surveyData.filter(
-                        (a) => a.answers[index].answer === option.text
-                      ).length
-                    }
-                  </Typography>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+        <Card key={index} variant="outlined" style={{ marginBottom: "16px" }}>
+          <CardContent>
+            <Typography variant="h6">{question.text}</Typography>
+            {question.type === "text" ? (
+              <div style={{ maxHeight: "200px", overflowY: "auto" }}>
+                {surveyData.map((answers, i) => (
+                  <div key={i}>
+                    <Typography>{answers.answers[index]?.answer}</Typography>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>
+                {question.answers.map((option, optionIndex) => (
+                  <div key={optionIndex} style={{ marginBottom: "8px" }}>
+                    <Typography>{option.text}</Typography>
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <LinearProgress
+                        variant="determinate"
+                        value={
+                          (getAnswerCount(option.text, index) /
+                            surveyData.length) *
+                          100
+                        }
+                        style={{ marginRight: "8px", flexGrow: 1 }}
+                      />
+                      <Typography>
+                        {getAnswerCount(option.text, index)}
+                      </Typography>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
