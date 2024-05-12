7cec7d19ae66d73fbd154ab9cd07b0a6440b89a5
Raul <raul.gonzalez1@alumnos.uneatlantico.es>
2024-05-12 20:02:40 +0200

    Format Dashboard

--- src/components/survey/Dashboard.jsx
+++ src/components/survey/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { SurveyForm } from "./SurveyForm";
 import { SurveyList } from "./SurveyList";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PollIcon from "@mui/icons-material/Poll";
+import AddIcon from "@mui/icons-material/Add";
 import {
   Container,
+  Divider,
   Drawer,
   Grid,
   List,
-  ListItem,
+  ListItemButton,
   ListItemText,
-  Typography,
 } from "@mui/material";
 
 export const Dashboard = ({ onLogin }) => {
@@ -20,41 +23,40 @@ export const Dashboard = ({ onLogin }) => {
 
   return (
     <Container maxWidth="lg" style={{ paddingTop: 20 }}>
-      <Grid container spacing={3}>
-        <Grid item xs={3}>
-          <Drawer variant="permanent" anchor="left">
-            <List>
-              {["Surveys", "New Survey"].map((option) => (
-                <ListItem
-                  key={option}
-                  onClick={() => handleOptionClick(option)}
-                  style={{ cursor: "pointer" }}
-                >
-                  <ListItemText primary={option} />
-                </ListItem>
-              ))}
-            </List>
-            <List style={{ marginTop: "auto" }}>
-              <ListItem onClick={onLogin} style={{ cursor: "pointer" }}>
-                <ListItemText primary="Logout" />
-              </ListItem>
-            </List>
-          </Drawer>
-        </Grid>
-        <Grid item xs={9}>
-          <Typography variant="h4" gutterBottom>
-            {selectedOption}
-          </Typography>
-          {selectedOption === "Surveys" && (
-            <div>
-              <SurveyList />
-            </div>
-          )}
-          {selectedOption === "New Survey" && (
-            <div>
-              <SurveyForm />
-            </div>
-          )}
+      <Drawer variant="permanent" anchor="left">
+        <List>
+          {["New Survey", "Surveys"].map((option) => (
+            <ListItemButton
+              key={option}
+              onClick={() => handleOptionClick(option)}
+              style={{
+                cursor: "pointer",
+                "&:hover": { backgroundColor: "#b6b6b6" },
+              }}
+            >
+              {option === "Surveys" ? <PollIcon /> : <AddIcon />}
+              <ListItemText style={{ marginLeft: 5 }} primary={option} />
+            </ListItemButton>
+          ))}
+        </List>
+        <List style={{ marginTop: "auto", padding: "5" }}>
+          <Divider variant="middle" />
+          <ListItemButton
+            onClick={onLogin}
+            style={{
+              cursor: "pointer",
+              "&:hover": { backgroundColor: "#b6b6b6" },
+            }}
+          >
+            <LogoutIcon />
+            <ListItemText style={{ marginLeft: 5 }} primary="Logout" />
+          </ListItemButton>
+        </List>
+      </Drawer>
+      <Grid container>
+        <Grid item xs={13} marginTop={10}>
+          {selectedOption === "Surveys" && <SurveyList />}
+          {selectedOption === "New Survey" && <SurveyForm />}
         </Grid>
       </Grid>
     </Container>
